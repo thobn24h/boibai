a218fb46f42822a8690463db30a3e6a55f6fa746
Bui Ngoc Tho <thobn24h@gmail.com>
2025-10-15 21:46:05 +0700

    update layout

--- assets/scripts/BoiNgayScene.ts
+++ assets/scripts/BoiNgayScene.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Label, Node } from 'cc';
+import { _decorator, Component, director, Label, Node, Sprite } from 'cc';
 import dayjs from 'dayjs';
 
 const { ccclass, property } = _decorator;
@@ -8,6 +8,27 @@ export class BoiNgayScene extends Component {
 
     @property(Label)
     labelBoiNgay: Label = null;
+
+    @property(Label)
+    lblDem: Label = null;
+
+    @property(Node)
+    lblThongbaoTraobai: Node = null;
+
+    @property(Node)
+    controlButtonLayoutNode: Node = null;
+
+    @property(Node)
+    card1: Node = null;
+
+    @property(Node)
+    card2: Node = null;
+
+    flag = false;
+    flagDangtraobai = false;
+    ngay = 0;
+
+    temp = 0; // danh dau z cua la bai khi trao bai
 
     protected onLoad(): void {
         const now = dayjs();
@@ -28,5 +49,37 @@ export class BoiNgayScene extends Component {
     update(deltaTime: number) {
         
     }
+
+    beginEffectTraobai() {
+      // hide message
+      this.lblThongbaoTraobai.active = false;
+      // hide menu back / next 
+      this.controlButtonLayoutNode.active = false;
+
+      // active card1/card2 for effect
+      this.card1.active = true;
+      this.card2.active = true;
+
+      
+
+    }
+
+    stopEffectTraobai() {
+
+    }
+
+    clickBack() {
+      director.loadScene("menuScene")
+    }
+
+    clickNext() {
+      if (this.flag) {
+        director.loadScene("menuScene")
+      }
+      else if (!this.flagDangtraobai) {
+        this.lblThongbaoTraobai.active = true;
+      }
+    }
+    
 }
 
