4a25b66bd01a24fad3c45dcdf886b876b6070b98
Bui Ngoc Tho <thobn24h@gmail.com>
2025-10-19 16:50:02 +0700

    update GUI

--- assets/scripts/BoiNgayPlayScene.ts
+++ assets/scripts/BoiNgayPlayScene.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, SpriteFrame } from 'cc';
+import { _decorator, Component, Layers, Node, Sprite, SpriteFrame, tween, UITransform, Vec3, screen, view } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('BoiNgayPlayScene')
@@ -9,6 +9,9 @@ export class BoiNgayPlayScene extends Component {
 
     @property(SpriteFrame)
     queSpriteFrame: SpriteFrame = null
+
+    @property(Node)
+    xapBaiNode: Node = null
 
     numberCardsDuoiFliped = 0;
 		flagNext = false;
@@ -30,10 +33,39 @@ export class BoiNgayPlayScene extends Component {
         
     }
 
+    createNewCardNode(tag: number, moveTime: number, startPos: Vec3, endPos: Vec3) {
+      const newNode = new Node()
+      newNode.layer = Layers.Enum.UI_2D
+      newNode.name = `${tag}`
+      
+      const sprite = newNode.addComponent(Sprite)
+      sprite.spriteFrame = this.queSpriteFrame
+
+      // const newNodeTransform = newNode.getComponent(UITransform)
+      // newNodeTransform.setContentSize(nodeSize)
+
+      newNode.setScale(new Vec3(0.55, 0.55, 0.55))
+      newNode.setPosition(startPos)
+
+      // add node to the container
+      this.canvasNode.addChild(newNode)
+
+      // create animation
+      tween(newNode).to(moveTime, endPos).start()
+    }
+
     effectChiabai() {
+      const startPos = this.xapBaiNode.getPosition()
+      const screenSize = view.getDesignResolutionSize()
+      console.log(`screenSize: ${screenSize.width} ${screenSize.height}`)
+
       if (this.demi <= 7 && this.demPTMang < 1028) {
         if (this.demj <= this.demi) {
           // create new sprite node
+          this.createNewCardNode(this.demPTMang, 0.2, 
+            startPos, 
+            new Vec3((this.demox + 46 * (this.demj - 1)) * 2 - screenSize.width / 2, this.demoy * 2 - screenSize.height / 2, 0)
+          )
 
           this.demj++;
 			    this.demPTMang++;
@@ -44,6 +76,11 @@ export class BoiNgayPlayScene extends Component {
           this.demoy = this.demoy - 20;
           this.demi++;
           // create new node
+          // create new sprite node
+          this.createNewCardNode(this.demPTMang, 0.2, 
+            startPos, 
+            new Vec3((this.demox + 46 * (this.demj - 1)) * 2 - screenSize.width / 2, this.demoy * 2 - screenSize.height / 2, 0)
+          )
 
           this.demj++;
 			    this.demPTMang++;
