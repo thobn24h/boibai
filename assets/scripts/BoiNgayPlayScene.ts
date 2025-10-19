116dbb4f34c802e56515028a20b2fd7ea4f2b2ec
Bui Ngoc Tho <thobn24h@gmail.com>
2025-10-19 15:59:04 +0700

    update logic

---
+++ assets/scripts/BoiNgayPlayScene.ts
@@ -0,0 +1,51 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('BoiNgayPlayScene')
+export class BoiNgayPlayScene extends Component {
+
+    numberCardsDuoiFliped = 0;
+		flagNext = false;
+		idDoi1 = 0;
+		idDoi2 = 0;
+		idDoi3 = 0;
+
+    demox = 162;
+		demoy = 350;
+		demi = 1;
+		demj = 1;
+		demPTMang = 1000;
+
+    start() {
+      this.schedule(this.effectChiabai, 0.2)
+    }
+
+    update(deltaTime: number) {
+        
+    }
+
+    effectChiabai() {
+      if (this.demi <= 7 && this.demPTMang < 1028) {
+        if (this.demj <= this.demi) {
+          // create new sprite node
+
+          this.demj++;
+			    this.demPTMang++;
+        }
+        else {
+          this.demj = 1;
+          this.demox = this.demox - 23;
+          this.demoy = this.demoy - 20;
+          this.demi++;
+          // create new node
+
+          this.demj++;
+			    this.demPTMang++;
+        }
+      }
+      else {
+        this.unschedule(this.effectChiabai)
+      }
+    }
+}
+
